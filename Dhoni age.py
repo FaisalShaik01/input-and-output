ecc12766df48680bcba19bb7d3e5794c97d7ff89
K.M.FAISAL <155710058+FaisalShaik01@users.noreply.github.com>
2024-10-01 10:10:12 +0530

    Update Dhoni age.py

--- Dhoni age.py
+++ Dhoni age.py
@@ -2,6 +2,11 @@
 MS Dhoni aged X years, is a Cancerian, born with very strong Mars in his birth chart.
 Notably, Mars is the ruling planet for sports.
 Write a program to get the age of Dhoni as an integer and display the same.
+Sample Input and Output:
+Enter the age:
+35
+Age of Dhoni is 35
+  
 ..
 a=int(input())
 print("Age of Dhoni is",a)
